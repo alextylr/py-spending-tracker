7aa7b95180c1bbbe3de1ad213536691e69140c70
Alexis Landry <alexis.landry@davita.com>
2020-03-10 21:21:17 -0500

    add caclulation!

--- spending-tracker.py
+++ spending-tracker.py
@@ -1,1 +1,30 @@
-uh 
+
+from csv import reader
+opened = open('Checking1.csv')
+read_file = reader(opened)
+checking_data = list(read_file)
+
+transactions = []
+merchants = []
+merchants_parsed = []
+
+
+for data in checking_data:
+    # get all the transaction amounts + the merchants
+    transactions.append(data[1])
+    merchants.append(data[-1])
+for i in range(len(merchants) -1):
+    merchants[i] = merchants[i].split(' ')
+    if 'PURCHASE' in merchants[i]:   
+      merchants[i] = merchants[i][4]
+
+      print(merchants)
+# for x in range(len(transactions)-1):
+#     transactions[x] = float(transactions[x])
+      
+       
+# total_spent = sum(transactions)
+
+# print("You've spent", total_spent)
+
+print(merchants)
