8ce3cabddf7f13f85a30dcd38311267c044bb66a
Alexis Landry <alexis.landry@davita.com>
2020-03-18 23:15:42 -0500

    added logic to get the vendor !

--- spending-tracker.py
+++ spending-tracker.py
@@ -1,5 +1,6 @@
 
 from csv import reader
+
 opened = open('Checking1.csv')
 read_file = reader(opened)
 checking_data = list(read_file)
@@ -7,16 +8,25 @@ checking_data = list(read_file)
 transactions = []
 merchants = []
 
-
+count = 0
 for data in checking_data:
     # get all the transaction amounts + the merchants
     transactions.append(data[1])
     merchants.append(data[-1])
+for i in range(len(merchants) -1):
+    merchants[i] = merchants[i].split(' ')
+    if 'PURCHASE' in merchants[i]:   
+        merchants[i] = merchants[i][4]
+    if merchants[i] == 'STARBUCKS' or 'CHIPOTLE':
+        count += 1
+        print(count)
+       
+
 
 # convert all transaction amounts from strings to floats
 list_of_transactions = [float(item) for item in transactions]
 
 
-
+# print(merchants)
 total_spent = sum(list_of_transactions)
-print("This month Allie spent a total of $", str(total_spent), "this month! Maybe she should stop eating so much Chipotle :)")
+print("This month Allie spent a total of $", str(total_spent), "this month!")
